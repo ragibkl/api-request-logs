53ec8229586034575e5e4cbeb3c7c3919fcad874
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2021-09-11 14:47:38 +0800

    add eslint ts and eslint airbnb ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,25 @@
 module.exports = {
+  root: true,
   env: {
     es2021: true,
     jest: true,
     node: true,
   },
-  extends: ['airbnb-base', 'prettier'],
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
+    project: './tsconfig.eslint.json',
   },
   plugins: ['@typescript-eslint'],
   rules: {
-    'import/no-extraneous-dependencies': [
-      'error',
-      { devDependencies: ['**/*.test.ts', '**/*.test.tsx'] },
-    ],
     'import/prefer-default-export': 'off',
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      { js: 'never', jsx: 'never', ts: 'never', tsx: 'never' },
-    ],
-  },
-  settings: {
-    'import/resolver': { node: { extensions: ['.js', '.jsx', '.ts', '.tsx'] } },
   },
 };
