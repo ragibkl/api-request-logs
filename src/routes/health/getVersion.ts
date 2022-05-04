b61566df21fb765df313942d1854db28d9cb0086
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2022-05-04 14:01:02 +0800

    version 2

--- src/routes/health/getVersion.ts
+++ src/routes/health/getVersion.ts
@@ -1,6 +1,6 @@
 import { RouterContext } from 'koa-router';
 
 export const getVersion = (ctx: RouterContext): void => {
-  ctx.body = { version: 1 };
+  ctx.body = { version: 2 };
   ctx.status = 200;
 };
